3ae68056947be3d2dd9b39a1f5661fc9a3fbba96
kawsarkabir <kawsarkabir11@gmail.com>
2022-07-31 22:31:48 +0600

    4ta problem slove in fast day

---
+++ Day-01/app.js
@@ -0,0 +1,39 @@
+// problem number 1
+// আমার কাছে আছে ১০০০ টাকা।
+var tkGiveHim = 1000;
+// অ্যাপেল কিনলাম ৩০০ টাকা দিয়া।
+var apple = 300;
+// অরেঞ্জ কিনলাম ৪০০ দিয়া।
+var orange = 400;
+// তাহলে অ্যাপেল + অরেঞ্জ কিনলাম (৩০০+৪০০) = ৭০০-১০০০ =৩০০ টাকা 
+takaAse = tkGiveHim - (apple + orange);
+// console.log(takaAse);
+
+
+
+// practice problem 2
+// 6ta sub ar avarage marks
+ var Mathematics = 75.25;
+ var Biology = 65;
+ var Chemistry = 80;
+ var Physics = 35.45;
+ var Bangla = 99.50;
+ totalMarks = (Mathematics + Biology + Chemistry + Physics + Bangla);
+ avarageMarks = totalMarks / 5;
+ var avarageMarks = avarageMarks.toFixed(2);
+//  console.log(avarageMarks);
+
+// problem 3
+// 2ta string ke 1 line a korte hobe 
+var line1 ="I am going to be";
+var line2 ="an awesome web developer"
+sentence = line1 + " " + line2 ;
+// console.log(sentence);
+
+// practice problem 4
+// riminder ber korar 
+var num = 119;
+var vagFol = 5;
+reminder = num % vagFol ;
+console.log(reminder);
+
