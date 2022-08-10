ecf31c27e96d44b5ebe6f8f9b9290124fe102e5a
kawsarkabir <kawsarkabir11@gmail.com>
2022-08-10 21:03:22 +0600

    day-02

---
+++ Day-02/problem-solving.js
@@ -0,0 +1,10 @@
+// এখানে বেনানার পজিসন বের করতে হবে , এন্ড Replace করতে হবে mango
+// problem num-1
+var fruits =["apple", "banana", "orange"];
+console.log(fruits.indexOf("banana"));
+fruits[1]= "mango"
+console.log(fruits);
+// orange কে এখন বের করে দিতে হবে এন্ড watermelon কে অ্যাড করতে হবে
+console.log(fruits.pop());
+console.log(fruits.push("komla"));
+console.log(fruits);
